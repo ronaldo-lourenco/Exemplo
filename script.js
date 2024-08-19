0bbd3d459db4b312e206965419b0505447971e6f
ronaldo-lourenco <ronaldo.lourenco@escola.pr.gov.br>
2024-08-19 14:46:38 -0300

    Projeto Final realizado no papo formativo GE

---
+++ script.js
@@ -0,0 +1,87 @@
+const caixaPrincipal = document.querySelector(".caixa-principal") ;
+const caixaPerguntas = document.querySelector(".caixa-perguntas") ;
+const caixaAlternativas = document.querySelector(".caixa-alternativas") ;
+const caixaResultado = document.querySelector(".caixa-resultado") ;
+const textoResultado = document.querySelector(".texto-resultado") ;
+
+const perguntas =[
+    {
+        enunciado : "Como você acredita que podemos combater o racismo na sociedade?",
+        alternativas : [
+            {
+            texto: "Participando de campanhas de conscientização e educação sobre igualdade racial.",
+            afirmacao: "Afirmação 1"
+            },
+            {
+            texto: "Denunciando atos de discriminação e apoiando políticas públicas inclusivas.",
+            afirmacao: "Afirmação 2"
+            }
+        ]
+    },
+    {
+        enunciado : "Como você pode ajudar a combater a disseminação de fake news, especialmente as que perpetuam o racismo?",
+        alternativas : [
+            {
+            texto: "Verificando a veracidade das informações antes de compartilhar qualquer conteúdo, especialmente sobre temas sensíveis como racismo.",
+            afirmacao: "Afirmação 1"
+            },
+            {
+            texto: "Educando amigos e familiares sobre os perigos das fake news e incentivando-os a não compartilhar conteúdos falsos que perpetuam o racismo.",
+            afirmacao: "Afirmação 2"
+            }
+        ]
+    },
+    {
+        enunciado : "Como podemos incentivar o uso de energia solar e outras formas de energia limpa, especialmente em comunidades carentes?",
+        alternativas : [
+            {
+            texto: "Promovendo a instalação de painéis solares em escolas e centros comunitários de comunidades marginalizadas.",
+            afirmacao: "Afirmação 1"
+            },
+            {
+            texto: "Defendendo políticas públicas que subsidiem a energia solar para famílias de baixa renda.",
+            afirmacao: "Afirmação 2"
+            }
+        ]
+    }
+] ;
+
+let atual = 0 ;
+let perguntaAtual ;
+let historiaFinal = "" ;
+
+function mostraPergunta(){
+    if (atual >= perguntas.length){
+        mostraResultado() ;
+        return ;
+    } ;
+    perguntaAtual                 = perguntas[atual] ;   
+    caixaPerguntas.textContent    = perguntaAtual.enunciado ;
+    caixaAlternativas.textContent = "" ;
+    textoResultado.textContent    = "" ;
+    mostraAlternativas() ;
+}
+
+function mostraAlternativas(){
+    for (const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativa = document.createElement("button") ;
+        botaoAlternativa.textContent = alternativa.texto ;
+        botaoAlternativa.addEventListener("click", ()=>(respostaSelecionada(alternativa))) ;  
+        caixaAlternativas.appendChild(botaoAlternativa) ;
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacao = opcaoSelecionada.afirmacao ;
+    historiaFinal   += afirmacao + " " ;
+    atual++ ;
+    mostraPergunta() ;
+} 
+
+function mostraResultado(){
+    caixaPerguntas.textContent    = "Suas ações mudam o mundo..." ;
+    caixaResultado.textContent    = historiaFinal ;
+    caixaAlternativas.textContent = "" ;
+}
+
+mostraPergunta() ;
